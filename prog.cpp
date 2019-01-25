4c5f8ae9aa1196ae6bbca9ad90cf886003849525
SHASHWAT KUMAR <500068392@DDN.UPES.AC.IN>
2019-01-25 21:43:37 +0530

    committing now

---
+++ prog.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+
+using namespace std;
+
+class my
+{
+public:
+    int a,b;
+    static int x;
+
+    void getData(void)
+    {
+        cout<<"Enter 2 numbers"<<endl;
+        cin>>a>>b;
+    }
+
+    void add(void)
+    {
+        cout<<"the sum is "<<(a+b)<<endl;
+    }
+    void subtract(void)
+    {
+        cout<<"the subtract is "<<(a-b)<<endl;
+    }
+    void multipy(void)
+    {
+        cout<<"the product is "<<(a*b)<<endl;
+    }
+    void quotient(void)
+    {
+        cout<<"the quotient is "<<(a/b)<<endl;
+    }
+};
+
+int main()
+{
+    ;
+    my obj;
+    obj.getData();
+    obj.add();
+    obj.subtract();
+    obj.multipy();
+    obj.quotient();
+    return 0;
+}
